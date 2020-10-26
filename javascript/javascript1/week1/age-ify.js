a4da1a89f075a14c30e9497724917efcddbc2a46
sofiiadidovych <sofiiadidovych@gmail.com>
2020-10-26 16:13:11 +0100

    Add space to the string

--- javascript/javascript1/week1/age-ify.js
+++ javascript/javascript1/week1/age-ify.js
@@ -2,4 +2,4 @@ let yearOfBirth = 1991;
 let yearFuture = 2027;
 let age = yearFuture - yearOfBirth;
 console.log(age);
-console.log("You will be " + age + "years old in " + yearFuture + ".")
+console.log("You will be " + age + " years old in " + yearFuture + ".")
