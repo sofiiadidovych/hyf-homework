daf9628eaa72f8a3479dc8533e7479e1fa50bc25
sofiiadidovych <sofiiadidovych@gmail.com>
2020-11-10 12:01:37 +0100

    Add travel exercise

---
+++ javascript/javascript1/week3/travel.js
@@ -0,0 +1,12 @@
+const travelInformation = {
+    speed: 50,
+    destinationDistance: 432,
+};
+function calcTravelTime(travelInformation) {
+    const time = travelInformation.destinationDistance / travelInformation.speed;
+    const hours = Math.floor(time);
+    const minutes = Math.round((time - hours) * 60);
+    return `${hours} hours and ${minutes} minutes` ;
+}
+const travelTime = calcTravelTime(travelInformation);
+console.log(travelTime);
