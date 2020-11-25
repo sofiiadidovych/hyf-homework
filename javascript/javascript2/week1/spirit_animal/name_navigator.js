651cb487cdc579bc28a38b9a30d03104801c34dd
sofiiadidovych <sofiiadidovych@gmail.com>
2020-11-25 18:17:31 +0100

    Add optinal event types

--- javascript/javascript2/week1/spirit_animal/name_navigator.js
+++ javascript/javascript2/week1/spirit_animal/name_navigator.js
@@ -1,15 +1,14 @@
 // Create an input element
 const inputField = document.createElement('input');
 inputField.setAttribute('type','text');
-inputField.setAttribute('name','username');
 inputField.setAttribute('placeholder','Enter your name');
-inputField.setAttribute('id','my-input');
+inputField.setAttribute('class','name_input');
 const spiritAnimalNavigator = document.getElementById('spirit-animal-navigator');
 spiritAnimalNavigator.appendChild(inputField);
 
 // Create a button
 const myButton = document.createElement('button');
-myButton.setAttribute('id','my-butn');
+myButton.setAttribute('class','button');
 myButton.innerText = 'Let`s go!';
 spiritAnimalNavigator.appendChild(myButton);
 
@@ -26,7 +25,7 @@ const spiritAnimals = [
     'The Rainbow Thunderbird','Giant Dung Beetles']; // inspired by "Fantastic Beasts"
 
 function getTheSpiritAnimal() {
-    const userName = document.getElementById('my-input').value.trim();
+    const userName = inputField.value.trim();
 
     if (userName === '') {
         info.innerText = 'Tell me your name, and I will tell you your spirit animal'
@@ -35,5 +34,23 @@ function getTheSpiritAnimal() {
         info.innerText = `${userName} - ${spiritAnimals[randomNumber]}`
     }
 }
+// Option #1: The button is clicked
+myButton.addEventListener('click', function() {
+    if (document.querySelector('#button_click_option').checked) {
+        getTheSpiritAnimal();
+    }
+});
 
-myButton.addEventListener('click', getTheSpiritAnimal);
+// Option #2: Hover over inputfield
+inputField.addEventListener('mouseover', function() {
+    if (document.querySelector('#inputfield_hover_option').checked) {
+        getTheSpiritAnimal();
+    }
+});
+
+// Option #3: When text is entered to the inputfield
+inputField.addEventListener('input', function() {
+    if (document.querySelector('#text_written_option').checked) {
+        getTheSpiritAnimal();
+    }
+})
