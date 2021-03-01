04ddacb34e7ec9ecaccd1e5d6049fa99e69dd5fd
sofiiadidovych <sofiiadidovych@gmail.com>
2021-03-01 13:32:08 +0100

    Handle feedback comments

--- react/week1/todo-app/src/App.js
+++ react/week1/todo-app/src/App.js
@@ -1,81 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function DateItem(props) {
-  let d = new Date();
-  d.setFullYear(props.date.year, props.date.month - 1, props.date.day);
+const DateItem = ({ date }) => {
+  const deadlineDate = new Date(date.year, date.month - 1, date.day);
   const today = new Date();
-  const validDate = (d > today) ? 'valid': 'expired';
+  const validDate = deadlineDate > today ? "valid" : "expired";
   return (
-    <p>{props.date.day}/ {props.date.month}/ {props.date.year} {`${validDate}`}</p>
-  )
+    <p>
+      {date.day}/ {date.month}/ {date.year} {`${validDate}`}
+    </p>
+  );
 }
 
-function TodoItem(props) {
+const TodoItem = ({ number, name, deadlineDate }) => {
   return (
     <li>
-      <h2>#{props.number}: {props.name}</h2>
-      <DateItem date={props.deadlineDate}></DateItem>
+      <h2>
+        #{number}: {name}
+      </h2>
+      <DateItem date={deadlineDate}></DateItem>
     </li>
-  )
+  );
 }
 
-function TodoList(props) {
+const TodoList = ({ todos }) => {
   return (
     <ul>
       <h1>Todo List</h1>
-      {
-        props.todos.map(todo => {
-          return <TodoItem number={todo.number} name={todo.name} deadlineDate={todo.deadlineDate}></TodoItem>
-        })
-      }
+      {todos.map((todo) => {
+        return (
+          <TodoItem
+            key={todo.number.toString()}
+            number={todo.number}
+            name={todo.name}
+            deadlineDate={todo.deadlineDate}
+          ></TodoItem>
+        );
+      })}
     </ul>
-  )
+  );
 }
 
 const todos = [
   {
     number: 1,
-    name: 'Get out of bed',
+    name: "Get out of bed",
     deadlineDate: {
       day: 14,
       month: 2,
-      year: 2021
-    }
+      year: 2021,
+    },
   },
   {
     number: 2,
-    name: 'Brush teeth',
+    name: "Brush teeth",
     deadlineDate: {
       day: 15,
       month: 3,
-      year: 2021
-    }
+      year: 2021,
+    },
   },
   {
     number: 3,
-    name: 'Eat breakfast',
+    name: "Eat breakfast",
     deadlineDate: {
       day: 16,
       month: 5,
-      year: 2021
-    }
+      year: 2021,
+    },
   },
   {
     number: 4,
-    name: 'Write a plan for a day',
+    name: "Write a plan for a day",
     deadlineDate: {
       day: 17,
       month: 6,
-      year: 2021
-    }
-  }
+      year: 2021,
+    },
+  },
 ];
 
 function App() {
-  return (
-    <TodoList todos={todos} />
-  );
+  return <TodoList todos={todos} />;
 }
 
 export default App;
